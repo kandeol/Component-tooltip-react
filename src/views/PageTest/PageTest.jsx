c21e3801d457c8a80a144984d8ae4e5ffc0980aa
kandeol <kandeol@student.42.fr>
2021-04-11 00:51:56 +0200

    add page test for test tooltip

---
+++ src/views/PageTest/PageTest.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Tooltip from "../../components/Tooltip/Tooltip";
+import "../../sass/page_test.scss";
+
+const PageTest = (params) => {
+    return (
+        <div className="page-test">
+            <div className="section-hover">
+                <span>
+                    Hover&nbsp;
+                    <Tooltip
+                        direction="top"
+                        type="hover"
+                        content="Lorem ipsum dolor sit amet"
+                    >
+                        <strong>HERE</strong>
+                    </Tooltip>
+                    &nbsp;!
+                </span>
+            </div>
+            <div className="section-focus">
+                <span>Example with input :&nbsp;</span>
+                <Tooltip
+                    direction="top"
+                    type="focus"
+                    content="Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
+                >
+                    <input type="text" placeholder="text input" />
+                </Tooltip>
+            </div>
+            <div className="section-component"></div>
+        </div>
+    );
+};
+
+export default PageTest;
